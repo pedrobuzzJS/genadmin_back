e25f4be92f9e0da1bf44682f651c960b0991b93f
pedrobuzzJS <pedro.auth.buzzi@gmail.com>
2023-06-26 13:21:08 -0300

    Alterando tratamento de erros

--- src/middleware/erro.ts
+++ src/middleware/erro.ts
@@ -9,14 +9,7 @@ export const errorMiddleware = (
   next: NextFunction  
 ) => {
 
-  // if (erro instanceof Prisma.PrismaClientKnownRequestError) {
-  //   // console.log(erro)
-  //   return response.status(404).json(erro);
-  // };
-
   if (erro) return response.status(500).json(erro)
-
-  // console.log(erro)
 
   const statusCode = erro.statusCode ?? 500;
   const message = erro.statusCode ? erro.message : "Internal Serve Error";
